df267f25233abfd7ee72d798d161f62f1b7cbb2a
tduong1443 <nguyenthuyduongk38@gmail.com>
2023-09-20 22:11:12 +0700

    first commit

---
+++ WebApplication1/Models/MenuItem.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models {
+	public class MenuItem {
+		public int Id { get; set; }//Item id
+		public string Name { get; set; } //Item name
+		public string Link { get; set; } //Item label
+	}
+}
---
+++ WebApplication1/ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewComponents {
+	public class RenderViewComponent : ViewComponent {
+		private List<MenuItem> menuItems = new List<MenuItem>();
+
+		public RenderViewComponent() {
+			this.menuItems = new List<MenuItem>() {
+				new MenuItem() {Id = 1, Name = "Branches", Link = "Branches/List"},
+				new MenuItem() {Id = 2, Name = "Students", Link = "Students/List"},
+				new MenuItem() {Id = 3, Name = "Subjects", Link = "Subjects/List"},
+				new MenuItem() {Id = 4, Name = "Courses", Link = "Courses/List"}
+			};
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync() {
+			return this.View("RenderLeftMenu", this.menuItems);
+		}
+	}
+}
